6aa7eaabd5b131418326a260b049228b73e1cd3b
Jason Letourneau <jletourneau80@gmail.com>
2012-05-16 09:50:21 -0400

    0508 logic added

--- measures/0580_bipolar_agent/0580_BipolarAgent.js
+++ measures/0580_bipolar_agent/0580_BipolarAgent.js
@@ -12,29 +12,46 @@ function() {
 
   var measurement_period_start = effective_date - (1 * year);
   var latest_birthdate = latestBirthdayForThisAge(17, measurement_period_start);
-  var earliest_encounter = patient.birthdate;
-  var latest_encounter = patient.birthdate + (.5 * year);
+  var earliest_encounter = measurement_period_start - (1 * year);
+  var exclusion_encounter = earliest_encounter - (1 * year);
 
   var population = function() {
-    var encounters=normalize(measure.encounter_office_visit_encounter_performed,
-                             measure.encounter_face_to_face_interaction_encounter_performed,
-                             measure.encounter_outpatient_consulatation_encounter_performed);
+    
 
-    var test = (patient.birthdate >= latest_birthdate);
-
-    return( (patient.birthdate >= latest_birthdate) &&
-             inRange(encounters, earliest_encounter, latest_encounter));
+    return(patient.birthdate <= latest_birthdate);
   }
 
   var denominator = function() {
-    return true;
+
+    var inpatient_encounters_inrange = selectWithinRange(measure.encounter_bh_inpatient_encounter_encounter_performed, earliest_encounter,measurement_period_start);
+    var emergency_encounters_inrange = selectWithinRange(measure.encounter_bh_emergency_department_visit_encounter_performed, earliest_encounter,measurement_period_start);
+    var outpatient_encounters_inrange = selectWithinRange(measure.encounter_bh_outpatient_encounter_encounter_performed, earliest_encounter,measurement_period_start);
+
+    var bipolar_disorder_inrange = actionFollowingSomething(outpatient_encounters_inrange, measure.condition_bh_bipolar_disorder_diagnosis_active , day);
+    var principal_bipolar_inrange = actionFollowingSomething(inpatient_encounters_inrange,measure.condition_bh_bipolar_disorder_diagnosis_active_principal_, day);
+    var principal_bipolar_emergency_inrange = actionFollowingSomething(emergency_encounters_inrange, measure.condition_bh_bipolar_disorder_diagnosis_active_principal_,day);
+    
+    var denominator = ((bipolar_disorder_inrange>=3) || (principal_bipolar_inrange>=1) || (principal_bipolar_emergency_inrange>=1));
+
+    var inpatient_encounters_outrange = selectWithinRange(measure.encounter_bh_inpatient_encounter_encounter_performed,exclusion_encounter, earliest_encounter);
+    var emergency_encounters_outrange = selectWithinRange(measure.encounter_bh_emergency_department_visit_encounter_performed, exclusion_encounter, earliest_encounter);
+    var outpatient_encounters_outrange = selectWithinRange(measure.encounter_bh_outpatient_encounter_encounter_performed, exclusion_encounter, earliest_encounter);
+
+    var bipolar_disorder_outrange = actionFollowingSomething(outpatient_encounters_outrange, measure.condition_bh_bipolar_disorder_diagnosis_active , day);
+    var principal_bipolar_outrange = actionFollowingSomething(inpatient_encounters_outrange,measure.condition_bh_bipolar_disorder_diagnosis_active_principal_, day);
+    var principal_bipolar_emergency_outrange = actionFollowingSomething(emergency_encounters_outrange, measure.condition_bh_bipolar_disorder_diagnosis_active_principal_,day);
+    
+    var exclusion = ((principal_bipolar_outrange>=1) || (bipolar_disorder_outrange>=1) || (principal_bipolar_emergency_outrange>=1));
+
+   
+    return  denominator && !exclusion;
   }
 
   var numerator = function() {
-    var pp_depression_screen = inRange(measure.risk_category_pp_depression_screening_risk_category_assessment,patient.birthdate, patient.birthdate + (.5 * year));
-    var pp_depression_treatment = inRange(measure.risk_category_depression_treatment_intervention_performed,patient.birthdate, patient.birthdate + (.5 * year));
-    
-    return pp_depression_screen || pp_depression_treatment;
+   var stabilizer = inRange(measure.medication_bh_mood_stabilizer_agent_medication_ordered,measurement_period_start,effective_date);
+    var outpatient_encounters_inrange = selectWithinRange(measure.encounter_bh_outpatient_encounter_encounter_performed, measurement_period_start,effective_date);
+    var stabilizer_active = actionFollowingSomething(outpatient_encounters_inrange, measure.medication_bh_mood_stabilizer_agent_medication_active , day); 
+    return stabilizer_ordered>=1 || stabilizer_active>=1;
   }
 
   var exclusion = function() {
